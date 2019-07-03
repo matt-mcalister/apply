57a3e2ab4924b81703da2c439e38ab5481113a51
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-03 11:11:59 -0400

    setup organization to pass initial tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190703145726) do
+ActiveRecord::Schema.define(version: 20190703151000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
