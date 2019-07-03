d98cedda50fa188d78b3d533644032f22537e05d
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-03 11:06:12 -0400

    wrote validation tests for user model, no relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validates presence of first name" do
+    should validate_presence_of(:first_name)
+  end
+  it "validates presence of last name" do
+    should validate_presence_of(:last_name)
+  end
+  it "validates presence of email" do
+    should validate_presence_of(:email)
+  end
+  it "validates uniqueness of email" do
+    should validate_uniqueness_of(:email)
+  end
+  it "validates format of email" do
+    should allow_value("email@address.horse").for(:email)
+    should_not allow_value("foo").for(:email)
+  end
 end
