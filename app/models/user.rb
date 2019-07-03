9241f3e39584c0afaae9c1139404bab0bc7305d3
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-03 11:09:22 -0400

    setup validations to pass initial tests, no relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
+  validate :email_format
+
+  private
+  def email_format
+    if email && !email.match(/\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/)
+      errors.add(:email, "must be in the format email@domain.tld")
+    end
+  end
 end
