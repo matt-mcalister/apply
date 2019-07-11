3ff7f7a7d26966d09c356360a1fc203d16ddec7c
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-11 17:27:33 -0400

    added status to applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190708181210) do
+ActiveRecord::Schema.define(version: 20190711212134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190708181210) do
     t.bigint "opportunity_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["opportunity_id"], name: "index_applications_on_opportunity_id"
     t.index ["user_role_id"], name: "index_applications_on_user_role_id"
   end
